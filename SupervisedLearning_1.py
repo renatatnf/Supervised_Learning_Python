03ec0ef49f24123f6f1962e5799dc5567b8582d8
Renata Fernandes <renatatnf@gmail.com>
2022-01-29 13:05:30 -0300

    Creation of notebook, organization of code

---
+++ SupervisedLearning_1.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 08:11:23 2022
+
+@author: renata.fernandes
+"""
+
+#CHAPTER 1 - CLASSIFICATION
+
+# Import necessary modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+
+# DATASET house-votes-84
+
+df = pd.read_csv('Dados/house-votes-84.csv', sep=',')
+
+#Pre-Processing
+
+df = df.replace("n",-1)
+df = df.replace("?",0)
+df = df.replace('y',1)
+
+#CHAPTER 1 - CLASSIFICATION
+
+
+plt.figure()
+sns.countplot(x='missile', hue='party', data=df, palette='RdBu', order = [-1, 0,  1] )
+plt.xticks([0,1,2], ['No', '?', 'Yes'])
+plt.show()
+
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
+
+# Create a k-NN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X_train,y_train)
+
+# Predict the labels for the training data X
+y_pred = knn.predict(X_test)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors = k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train,y_train)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train,y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.title('VOTES -k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+# DATASET load_digits
+
+# Load the digits dataset: digits
+digits = datasets.load_digits()
+
+# Print the keys and DESCR of the dataset
+print(digits.keys())
+print(digits.DESCR)
+
+# Print the shape of the images and data keys
+print(digits.images.shape)
+print(digits.data.shape)
+
+# Display digit 1010
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+# Create feature and target arrays
+X = digits.data
+y = digits.target
+
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
+
+# Create a k-NN classifier with 7 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=7)
+
+# Fit the classifier to the training data
+knn.fit(X_train,y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors = k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train,y_train)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train,y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.title('DIGITS - k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+
