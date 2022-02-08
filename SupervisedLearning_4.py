1133f6299b35ecacd8b4ae5b01a1a06dfa0053a8
Renata Fernandes <renatatnf@gmail.com>
2022-02-08 09:41:41 -0300

    Update SupervisedLearning_4.py

--- SupervisedLearning_4.py
+++ SupervisedLearning_4.py
@@ -14,8 +14,11 @@ from sklearn.model_selection import cross_val_score
 from sklearn.impute import SimpleImputer
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report
+from sklearn.preprocessing import scale, StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import ElasticNet
 
 #Exploring categorical features
 
@@ -125,103 +128,111 @@ y_pred = pipeline.predict(X_test)
 print(classification_report(y_test, y_pred))
 
 
+#Centering and scaling your data
 
-# #Centering and scaling your data
-# # Import scale
-# from sklearn.preprocessing import scale
+# Reading data
+# df = pd.read_csv('Dados/winequality-red.csv', sep=';')
+df = pd.read_csv('Dados/white-wine.csv', sep=',')
+# Create arrays for the features and the response variable
+y = df['quality'].values
+X = df.drop('quality', axis=1).values
 
-# # Scale the features: X_scaled
-# X_scaled = scale(X)
+# Scale the features: X_scaled
+X_scaled = scale(X)
 
-# # Print the mean and standard deviation of the unscaled features
-# print("Mean of Unscaled Features: {}".format(np.mean(X))) 
-# print("Standard Deviation of Unscaled Features: {}".format(np.std(X)))
+# Print the mean and standard deviation of the unscaled features
+print("Mean of Unscaled Features: {}".format(np.mean(X))) 
+print("Standard Deviation of Unscaled Features: {}".format(np.std(X)))
 
-# # Print the mean and standard deviation of the scaled features
-# print("Mean of Scaled Features: {}".format(np.mean(X_scaled))) 
-# print("Standard Deviation of Scaled Features: {}".format(np.std(X_scaled)))
+# Print the mean and standard deviation of the scaled features
+print("Mean of Scaled Features: {}".format(np.mean(X_scaled))) 
+print("Standard Deviation of Scaled Features: {}".format(np.std(X_scaled)))
 
 
+#Centering and scaling in a pipeline
 
-# #Centering and scaling in a pipeline
-# # Import the necessary modules
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.pipeline import Pipeline
-
-# # Setup the pipeline steps: steps
-# steps = [('scaler', StandardScaler()),
-#         ('knn', KNeighborsClassifier())]
+# Setup the pipeline steps: steps
+steps = [('scaler', StandardScaler()),
+        ('knn', KNeighborsClassifier())]
         
-# # Create the pipeline: pipeline
-# pipeline = Pipeline(steps)
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
 
-# # Create train and test sets
-# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_state=42)
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_state=42)
 
-# # Fit the pipeline to the training set: knn_scaled
-# knn_scaled = pipeline.fit(X_train, y_train)
+# Fit the pipeline to the training set: knn_scaled
+knn_scaled = pipeline.fit(X_train, y_train)
 
-# # Instantiate and fit a k-NN classifier to the unscaled data
-# knn_unscaled = KNeighborsClassifier().fit(X_train, y_train)
+# Instantiate and fit a k-NN classifier to the unscaled data
+knn_unscaled = KNeighborsClassifier().fit(X_train, y_train)
 
-# # Compute and print metrics
-# print('Accuracy with Scaling: {}'.format(knn_scaled.score(X_test,y_test)))
-# print('Accuracy without Scaling: {}'.format(knn_unscaled.score(X_test,y_test)))
-
-
-
-# #Bringing it all together I: Pipeline for classification
-# # Setup the pipeline
-# steps = [('scaler', StandardScaler()),
-#          ('SVM', SVC())]
-
-# pipeline = Pipeline(steps)
-
-# # Specify the hyperparameter space
-# parameters = {'SVM__C':[1, 10, 100],
-#               'SVM__gamma':[0.1, 0.01]}
-
-# # Create train and test sets
-# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=21)
-
-# # Instantiate the GridSearchCV object: cv
-# cv = GridSearchCV(pipeline, parameters, cv=3)
-
-# # Fit to the training set
-# cv.fit(X_train, y_train)
-
-# # Predict the labels of the test set: y_pred
-# y_pred = cv.predict(X_test)
-
-# # Compute and print metrics
-# print("Accuracy: {}".format(cv.score(X_test, y_test)))
-# print(classification_report(y_test, y_pred))
-# print("Tuned Model Parameters: {}".format(cv.best_params_))
+# Compute and print metrics
+print('Accuracy with Scaling: {}'.format(knn_scaled.score(X_test,y_test)))
+print('Accuracy without Scaling: {}'.format(knn_unscaled.score(X_test,y_test)))
 
 
+#Bringing it all together I: Pipeline for classification
+# Setup the pipeline
+steps = [('scaler', StandardScaler()),
+          ('SVM', SVC())]
 
-# #Bringing it all together II: Pipeline for regression
-# # Setup the pipeline steps: steps
-# steps = [('imputation', Imputer(missing_values='NaN', strategy='mean', axis=0)),
-#          ('scaler', StandardScaler()),
-#          ('elasticnet', ElasticNet())]
+pipeline = Pipeline(steps)
 
-# # Create the pipeline: pipeline 
-# pipeline = Pipeline(steps)
+# Specify the hyperparameter space
+parameters = {'SVM__C':[1, 10, 100],
+              'SVM__gamma':[0.1, 0.01]}
 
-# # Specify the hyperparameter space
-# parameters = {'elasticnet__l1_ratio':np.linspace(0,1,30)}
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=21)
 
-# # Create train and test sets
-# X_train, X_test, y_train, y_test =  train_test_split(X,y,test_size=0.4, random_state=42)
+# Instantiate the GridSearchCV object: cv
+cv = GridSearchCV(pipeline, parameters, cv=3)
 
-# # Create the GridSearchCV object: gm_cv (cv=3 - default)
-# gm_cv = GridSearchCV(pipeline, parameters)
+# Fit to the training set
+cv.fit(X_train, y_train)
 
-# # Fit to the training set
-# gm_cv.fit(X_train, y_train)
+# Predict the labels of the test set: y_pred
+y_pred = cv.predict(X_test)
 
-# # Compute and print the metrics
-# r2 = gm_cv.score(X_test, y_test)
-# print("Tuned ElasticNet Alpha: {}".format(gm_cv.best_params_))
-# print("Tuned ElasticNet R squared: {}".format(r2))
+# Compute and print metrics
+print("Accuracy: {}".format(cv.score(X_test, y_test)))
+print(classification_report(y_test, y_pred))
+print("Tuned Model Parameters: {}".format(cv.best_params_))
+
+
+#Bringing it all together II: Pipeline for regression
+ELNET_MAX_ITER = 100000
+ELNET_tolerance = 0.5
+# Setup the pipeline steps: steps
+steps = [('imputation', SimpleImputer(missing_values=np.nan, strategy='mean')),
+          ('scaler', StandardScaler()),
+          ('elasticnet', ElasticNet(max_iter= ELNET_MAX_ITER, tol=ELNET_tolerance))]
+
+# Create the pipeline: pipeline 
+pipeline = Pipeline(steps)
+
+# Specify the hyperparameter space
+parameters = {'elasticnet__l1_ratio':np.linspace(0,1,30)}
+
+# Read 'gapminder.csv' into a DataFrame: df
+df = pd.read_csv('Dados/gm_2008_region.csv')
+# Create dummy variables: df_region
+df_region = pd.get_dummies(df)
+
+y = df_region['life'].values
+X = df_region.drop(columns=['life'])
+
+# Create train and test sets
+X_train, X_test, y_train, y_test =  train_test_split(X,y,test_size=0.4, random_state=42)
+
+# Create the GridSearchCV object: gm_cv (cv=3 - default)
+gm_cv = GridSearchCV(pipeline, parameters)
+
+# Fit to the training set
+gm_cv.fit(X_train, y_train)
+
+# Compute and print the metrics
+r2 = gm_cv.score(X_test, y_test)
+print("Tuned ElasticNet Alpha: {}".format(gm_cv.best_params_))
+print("Tuned ElasticNet R squared: {}".format(r2))
